1f787d327c9b9e3418e08da6d21bec50f1092e6e
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-20 02:54:52 +0700

    create mock data

---
+++ src/data/index.js
@@ -0,0 +1,56 @@
+const data = [
+  {
+    id: 1,
+    icon: "⭕️",
+    status: "to do",
+    title: "Learn Golang",
+    content: "Create REST API with zero dependencies using go",
+  },
+  {
+    id: 2,
+    icon: "⭕️",
+    status: "to do",
+    title: "Learn Tensorflow js",
+    content:
+      "create machine learning model to classify credit card application",
+  },
+  {
+    id: 3,
+    icon: "⭕️",
+    status: "to do",
+    title: "Learn Vue3",
+    content: "use Vue 3 features like composition API in a project",
+  },
+  {
+    id: 4,
+    icon: "⭕️",
+    status: "to do",
+    title: "Push Rank",
+    content: "Push Rank to Legend",
+  },
+];
+
+const statuses = [
+  {
+    status: "to do",
+    icon: "⭕️",
+    color: "#EB5A46",
+  },
+  {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0",
+  },
+  {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0",
+  },
+  {
+    status: "done",
+    icon: "✅",
+    color: "#3981DE",
+  },
+];
+
+export { data, statuses };
