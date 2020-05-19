9ff528a45fe0bb7a6daae5d479878c87ee26b9bf
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-20 02:57:23 +0700

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className={"row"}>
+      <p className={"header"}>Kanban Board 🗂</p>
+    </div>
+  );
+};
+
+export default Header;
